c4415597b40b42bd907f821ecb4663fb827d83f7
David Nicholson <me@djnicholson.com>
2022-01-09 21:47:06 -0800

    working IPFS

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+import * as ipfs from "ipfs-core";
+
+(async () => {
+  const node = await ipfs.create();
+
+  const data = "Hello, <YOUR NAME HERE>";
+
+  // add your data to to IPFS - this can be a string, a Buffer,
+  // a stream of Buffers, etc
+  const results = await node.add(data);
+
+  // CID (Content IDentifier) uniquely addresses the data
+  // and can be used to get it again.
+  console.log(results.cid.toString());
+
+  await node.stop();
+})();
